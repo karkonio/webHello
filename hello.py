bcaedaeca4b3277d49a5ca62e8c771152666022e
Karina Yesbukenova <karina.yesbukenova@gmail.com>
2018-12-10 21:39:00 +0600

    Add POST requests for client's Cart

--- hello.py
+++ hello.py
@@ -1,10 +1,25 @@
+import json
+
 from flask import Flask
+from flask import render_template
+from flask import request
+
+
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return "Hello, World!"
+    with open('db.txt', 'r') as f:
+        items = json.load(f)
+        if request.method == 'POST':
+            item = request.form['item']
+            quantity = request.form['quantity']
+            items.update({item: quantity})
+            with open('db.txt', 'w') as f2:
+                json.dump(items, f2)
+    return render_template('hello.html', items=items)
+
 
 @app.route("/bye")
 def bye():
