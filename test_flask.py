c1a968ba9997877d6f7a57eaa48d97f3f2581f40
Karina Yesbukenova <karina.yesbukenova@gmail.com>
2018-12-11 13:29:17 +0600

    Add fixture for asserts

--- test_flask.py
+++ test_flask.py
@@ -1,18 +1,24 @@
+import pytest
+
 from io import StringIO
 from unittest import mock
 
 from hello import app # из файла hello.py 
 
 
-def test_index(): # первый текст для проверки корневого адреса
-    client = app.test_client() 
+@pytest.fixture
+def client():
+    client = app.test_client() # для обнаружения пайтестом
+    return client
+
+
+def test_index(client): # первый текст для проверки корневого адреса
     response = client.get('/')
     response = response.data.decode('utf-8') # для нормальной презентации содержимого
     assert 'Hello' in response
 
 
-def test_index(): # второй assert для проверки что по адресу ip/items записан словарь
-    client = app.test_client() # для обнаружения пайтестом
+def test_index(client): # второй assert для проверки что по адресу ip/items записан словарь
     with mock.patch('hello.open') as mocked: # open as очень сложный, поэтому используем для открытия БД mock
         mocked.return_value = StringIO('{"test": 1}') # описываем какого формата должны быть данные в .html
         response = client.get('/items')
