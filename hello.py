40354a474226bf86163562e90bc08122338bc434
Karina Yesbukenova <karina.yesbukenova@gmail.com>
2018-12-11 11:59:08 +0600

    Add new .html files and new /items way

--- hello.py
+++ hello.py
@@ -8,8 +8,13 @@ from flask import request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def hello():
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/items', methods=['GET', 'POST'])
+def items():
     with open('db.txt', 'r') as f:
         items = json.load(f)
         if request.method == 'POST':
@@ -18,9 +23,7 @@ def hello():
             items.update({item: quantity})
             with open('db.txt', 'w') as f2:
                 json.dump(items, f2)
-    return render_template('hello.html', items=items)
+    return render_template('items.html', items=items)
 
 
-@app.route("/bye")
-def bye():
-    return "Bye, World!"
+
