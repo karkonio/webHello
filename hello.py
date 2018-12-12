8488271fc3ce899f3e2d356f965100c94244fdc7
Karina Yesbukenova <karina.yesbukenova@gmail.com>
2018-12-12 23:55:23 +0600

    Add function as remove and change, but not as required

--- hello.py
+++ hello.py
@@ -18,12 +18,25 @@ def items():
     with open('db.txt', 'r') as f:
         items = json.load(f)
         if request.method == 'POST':
-            item = request.form['item']
-            quantity = request.form['quantity']
-            items.update({item: quantity})
+            add_item = request.form['item']
+            remove_item = request.form['remove_item']
+            old_item = request.form['old_item']
+            change_quan = request.form['new_quan']
+            if add_item:
+                item = request.form['item']
+                quantity = request.form['quantity']
+                items.update({item: quantity})
+            elif remove_item:
+                del items[remove_item]
+            elif old_item:
+                new_item = request.form['new_item']
+                items[new_item] = items[old_item]
+                del items[old_item]
+            elif change_quan:
+                item_name = request.form['item_name']
+                items[item_name] = change_quan
             with open('db.txt', 'w') as f2:
                 json.dump(items, f2)
     return render_template('items.html', items=items)
-
-
-
+# input type="hidden"
+# input type="checkbox"
--- test_flask.py
+++ test_flask.py
@@ -1,26 +1,35 @@
 import pytest
-
 from io import StringIO
 from unittest import mock
-
-from hello import app # из файла hello.py 
+from hello import app
 
 
 @pytest.fixture
 def client():
-    client = app.test_client() # для обнаружения пайтестом
+    client = app.test_client()
     return client
 
 
-def test_index(client): # первый текст для проверки корневого адреса
+def test_index(client):
     response = client.get('/')
-    response = response.data.decode('utf-8') # для нормальной презентации содержимого
+    response = response.data.decode('utf-8')
     assert 'Hello' in response
 
 
-def test_index(client): # второй assert для проверки что по адресу ip/items записан словарь
-    with mock.patch('hello.open') as mocked: # open as очень сложный, поэтому используем для открытия БД mock
-        mocked.return_value = StringIO('{"test": 1}') # описываем какого формата должны быть данные в .html
+def test_get_items(client):
+    with mock.patch('hello.open') as mocked:
+        mocked.return_value = StringIO('{"test": 1}')
         response = client.get('/items')
+        response = response.data.decode('utf-8')
+        assert '<li>test: 1</li>' in response
+
+
+def test_post_items(client):
+    with mock.patch('hello.open') as mocked:
+        mocked.return_value = StringIO('{}')
+        response = client.post(
+            '/items',
+            data={'item': 'test', 'quantity': 1}
+        )
         response = response.data.decode('utf-8')
         assert '<li>test: 1</li>' in response
