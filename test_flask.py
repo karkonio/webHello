55a67d6a4a412ff11f442ce2eba282c43ffe6f07
Karina Yesbukenova <karina.yesbukenova@gmail.com>
2018-12-20 19:20:40 +0600

    New tests to GET and POST requests(but only 84% coverage)

--- test_flask.py
+++ test_flask.py
@@ -13,7 +13,7 @@ def client():
 def test_index(client):
     response = client.get('/')
     response = response.data.decode('utf-8')
-    assert 'Hello' in response
+    assert 'Hello, my dear customer.' in response
 
 
 def test_get_items(client):
@@ -21,15 +21,24 @@ def test_get_items(client):
         mocked.return_value = StringIO('{"test": 1}')
         response = client.get('/items')
         response = response.data.decode('utf-8')
-        assert '<li>test: 1</li>' in response
+        assert 'value="test"' and 'value="1"' in response
 
 
-def test_post_items(client):
+def test_post_items_add(client):
     with mock.patch('hello.open') as mocked:
-        mocked.return_value = StringIO('{}')
+        mocked.return_value = StringIO('{"_": 0}')
         response = client.post(
             '/items',
-            data={'item': 'test', 'quantity': 1}
+            data={'{{ item }}': 'test', '{{ item }}_quantity': '1'}
         )
         response = response.data.decode('utf-8')
-        assert '<li>test: 1</li>' in response
+        assert 'value="test"' and 'value="1"' in response
+
+
+def test_post_items_remove(client):
+    with mock.patch('hello.open') as mocked:
+        mocked.return_value = StringIO('{"test": 1}')
+        response = client.post(
+            '/items', data={})
+        response = response.data.decode('utf-8')
+        assert '<input type="submit" value="Update">' in response
