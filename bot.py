621d6148f8727420cf1fb312ce3a0bb60725c9d1
Karina Yesbukenova <karina.yesbukenova@gmail.com>
2019-01-31 15:23:26 +0600

    Add /name

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from playhouse.shortcuts import model_to_dict
 
 from models import Item, Cart, CartItem, Customer
 
@@ -15,25 +15,19 @@ updater = Updater(token='704148322:AAE7A1WBjsovkDCbE6u-qwCI7CLASyXjm2M')
 dispatcher = updater.dispatcher
 
 
-def build_menu(buttons, n_cols):
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-    return menu
-
-
 def start(bot, update):
-    button_list = [
-        InlineKeyboardButton("col1", callback_data="1"),
-        InlineKeyboardButton("col2", callback_data="2"),
-        InlineKeyboardButton("row 2", callback_data="3")
-    ]
-    reply_markup = InlineKeyboardMarkup(
-        build_menu(button_list, n_cols=2)
-    )
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text="Добро пожаловать! Ознакомьтесь с командами :)",
-        reply_markup=reply_markup
-    )
+    try:
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text='Здравствуйте! Как вас Зовут?'
+            'Используйте команду /name + Ваше имя'
+        )
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text='Fail {}'.format(e)
+        )
 
 
 def buy(bot, update, args):
@@ -68,25 +62,32 @@ def buy(bot, update, args):
         )
 
 
-def customer(bot, update, args):
-    try:
-        name = args[0]
-        age = args[1]
+def name(bot, update, args):
+    """
+    Данная функция проверяет наличие пользователя в БД по имени.
+    Если такого пользователя нет, вносит его в БД.
+
+    """
+    name = args[0]
+    customers = Customer.select(Customer.name)
+    customers = [model_to_dict(customer) for customer in customers]
+    customers_list = []
+    for customer in customers:
+        customers_list.append(customer['name'])
+    if name in customers_list:
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text='{}, мы нашли вас в базе.'.format(name)
+        )
+    elif name not in customers_list:
         customer = Customer(
             name=name,
-            age=age
+            age=20
         )
         customer.save()
         bot.send_message(
             chat_id=update.message.chat_id,
-            text='OK, {}! You are in database and can shopping now :)'
-            .format(name)
-        )
-    except Exception as e:
-        logging.error(e, exc_info=True)
-        bot.send_message(
-            chat_id=update.message.chat_id,
-            text='Fail {}'.format(e)
+            text='Отлично, {}. Мы добавили вас в базу.'.format(name)
         )
 
 
@@ -106,12 +107,12 @@ def unknown(bot, update):
 
 start_handler = CommandHandler('start', start)
 buy_handler = CommandHandler('buy', buy, pass_args=True)
-customer_handler = CommandHandler('customer', customer, pass_args=True)
+name_handler = CommandHandler('name', name, pass_args=True)
 echo_handler = MessageHandler(Filters.text, echo)
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(buy_handler)
-dispatcher.add_handler(customer_handler)
+dispatcher.add_handler(name_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(unknown_handler)
 
